4dd7ca54c4ae8e0bdf34525ec1a4c6ce33af43fc
kaanbayram <kaan.9898@hotmail.com>
2021-08-07 16:18:55 +0300

    new change test from mac!

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
-import Footer from "./Footer";
+import { Footer, NavMenu } from "./";
 export class Layout extends Component {
   static displayName = Layout.name;
 
