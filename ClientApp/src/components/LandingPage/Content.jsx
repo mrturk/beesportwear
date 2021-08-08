710430f9ea4d718946e4d2e11d652b08e4ab81e5
kaanbayram <kaan.9898@hotmail.com>
2021-08-08 20:55:21 +0300

    last changes

--- ClientApp/src/components/LandingPage/Content.jsx
+++ ClientApp/src/components/LandingPage/Content.jsx
@@ -91,6 +91,224 @@ const Content = () => {
                     </div>
                 </div>
             </section>
+
+            <section className="call-to-action section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <h2>Let's Create Something Together</h2>
+                        <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicudin bibendum auctor,  nisi elit
+						consequat ipsum, nesagittis sem nid elit. Duis sed odio sitain elit.</p>
+                        <a href="" className="btn btn-main">Contact Us</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="counter-wrapper section-sm">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<div class="title">
+						<h2>Award-Winning Agency</h2>
+						<p>Vestibulum nisl tortor, consectetur quis imperdiet bibendum, laoreet sed arcu. Sed
+							condimentum iaculis ex, in faucibus lorem accumsan non. Donec mattis tincidunt metus. Morbi
+							sed tortor a risus luctus dignissim.</p>
+					</div>
+				</div>
+				
+				<div class="col-md-3 col-sm-6 col-xs-6 text-center ">
+					<div class="counters-item">
+						<i class="tf-ion-ios-alarm-outline"></i>
+						<div>
+							<span class="counter" data-count="150">0</span>
+						</div>
+						<h3>Happy Clients</h3>
+					</div>
+				</div>
+				
+				<div class="col-md-3 col-sm-6 col-xs-6 text-center ">
+					<div class="counters-item">
+						<i class="tf-ion-ios-analytics-outline"></i>
+						<div>
+							<span class="counter" data-count="130">0</span>
+						</div>
+						<h3>Projects completed</h3>
+					</div>
+				</div>
+				
+				<div class="col-md-3 col-sm-6 col-xs-6 text-center ">
+					<div class="counters-item">
+						<i class="tf-ion-ios-compose-outline"></i>
+						<div>
+							<span class="counter" data-count="99">0</span>
+						</div>
+						<h3>Positive feedback</h3>
+
+					</div>
+				</div>
+				
+
+				
+				<div class="col-md-3 col-sm-6 col-xs-6 text-center ">
+					<div class="counters-item kill-border">
+						<i class="tf-ion-ios-bolt-outline"></i>
+						<div>
+							<span class="counter" data-count="250">0</span>
+						</div>
+						<h3>Cups of Coffee</h3>
+					</div>
+				</div>
+				
+			</div> 
+		</div> 
+	</section> 
+
+    {/* <section class="testimonial section" id="testimonial">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="testimonial-slider">
+					
+						<div class="item text-center">
+							<i class="tf-ion-chatbubbles"></i>
+							
+							<div class="client-details">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nulla, soluta dolorum.
+									Eos earum, magni asperiores, unde corporis labore, enim, voluptatum officiis
+									voluptates alias natus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+									Quia, officia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, quia?
+								</p>
+							</div>
+							
+							<div class="client-thumb">
+								<img src="images/client-logo/clients-1.jpg" class="img-fluid" alt=""/>
+							</div>
+							<div class="client-meta">
+								<h3>William Martin</h3>
+								<span>CEO , Company Name</span>
+							</div>
+							
+						</div>
+					
+						<div class="item text-center">
+							<i class="tf-ion-chatbubbles"></i>
+						
+							<div class="client-details">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nulla, soluta dolorum.
+									Eos earum, magni asperiores, unde corporis labore, enim, voluptatum officiis
+									voluptates alias natus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+									Quia, officia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, quia?
+								</p>
+							</div>
+							
+							<div class="client-thumb">
+								<img src="images/client-logo/clients-2.jpg" class="img-fluid" alt=""/>
+							</div>
+							<div class="client-meta">
+								<h3>Emma Harrison</h3>
+								<span>CEO , Company Name</span>
+							</div>
+							
+						</div>
+					
+						<div class="item text-center">
+							<i class="tf-ion-chatbubbles"></i>
+							
+							<div class="client-details">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum nulla, soluta dolorum.
+									Eos earum, magni asperiores, unde corporis labore, enim, voluptatum officiis
+									voluptates alias natus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+									Quia, officia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, quia?
+								</p>
+							</div>
+							
+							<div class="client-thumb">
+								<img src="images/client-logo/clients-3.jpg" class="img-fluid" alt=""/>
+							</div>
+							<div class="client-meta">
+								<h3>Alexander Lucas</h3>
+								<span>CEO , Company Name</span>
+							</div>
+							
+						</div>
+						
+					</div>
+				</div> 
+			</div> 
+		</div> 
+	</section>  */}
+
+	
+
+	<section class="blog" id="blog">
+		<div class="container">
+			<div class="row">
+
+			
+				<div class="col-12">
+					<div class="title text-center ">
+						<h2> Latest <span class="color">Posts</span></h2>
+						<div class="border"></div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus facere accusamus,
+							reprehenderit libero
+							inventore nam.</p>
+					</div>
+				</div>
+			
+				<article class="col-md-4 col-sm-6 col-xs-12 clearfix ">
+					<div class="post-item">
+						<div class="media-wrapper">
+							<img src="images/blog/post-1.jpg" alt="amazing caves coverimage" class="img-fluid"/>
+						</div>
+
+						<div class="content">
+							<h3><a href="single-post.html">Reasons to Smile</a></h3>
+							<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+								squid. 3 wolf
+								moon officia aute, non skateboard dolor brunch.</p>
+							<a class="btn btn-main" href="single-post.html">Read more</a>
+						</div>
+					</div>
+				</article>
+				
+				<article class="col-md-4 col-sm-6 col-xs-12 ">
+					<div class="post-item">
+						<div class="media-wrapper">
+							<img src="images/blog/post-2.jpg" alt="amazing caves coverimage" class="img-fluid"/>
+						</div>
+
+						<div class="content">
+							<h3><a href="single-post.html">A Few Moments</a></h3>
+							<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+								squid. 3 wolf
+								moon officia aute, non skateboard dolor brunch.</p>
+							<a class="btn btn-main" href="single-post.html">Read more</a>
+						</div>
+					</div>
+				</article>
+				
+				<article class="col-md-4 col-sm-6 col-xs-12 ">
+					<div class="post-item">
+						<div class="media-wrapper">
+							<img src="images/blog/post-3.jpg" alt="amazing caves coverimage" class="img-fluid"/>
+						</div>
+
+						<div class="content">
+							<h3><a href="single-post.html">Hints for Life</a></h3>
+							<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+								squid. 3 wolf
+								moon officia aute, non skateboard dolor brunch.</p>
+							<a class="btn btn-main" href="single-post.html">Read more</a>
+						</div>
+					</div>
+				</article>  
+				
+			</div> 
+		</div> 
+	</section> 
+
         </React.Fragment >
     );
 }
