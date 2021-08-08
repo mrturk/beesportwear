67d29ca0a8f8a30c7c982f46b631fffd7940393a
Yunus <yunus1481emre@gmail.com>
2021-08-08 23:38:05 +0300

    login backend done

---
+++ Controllers/User.cs
@@ -0,0 +1,31 @@
+﻿using beesportwear.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace beesportwear.Controllers
+{
+    [ApiController]
+    public class User : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]/login")]
+        public IActionResult login(Users gelen)
+        {
+            Conn database = new Conn();
+            if (database.Login(gelen)=="true")
+            {
+                return Ok();
+            }
+            else
+            {
+                return Unauthorized();
+            }
+
+        }
+    }
+}
