3f065db47eeb4aa3518f71ecc58b78e5e0f0c613
Yunus <yunus1481emre@gmail.com>
2021-08-10 01:31:55 +0300

    update

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,14 +4,13 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
-const rootElement = document.getElementById("root");
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
+   <BrowserRouter basename={baseUrl}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
   rootElement
 );
 
--- ClientApp/src/pages/Home.js
+++ ClientApp/src/pages/Home.js
@@ -58,6 +58,126 @@ export default function Home() {
           <span class="sr-only">Next</span>
         </a>
       </div>
+      <section class="blog" id="blog">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="title text-center ">
+                <h2>
+                  {" "}
+                  BAŞLICA <span class="color">KATEGORİLER</span>
+                </h2>
+                <div class="border"></div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Ducimus facere accusamus, reprehenderit libero inventore nam.
+                </p>
+              </div>
+            </div>
+            <article class="col-md-3 col-sm-6 col-xs-12 clearfix ">
+              <div class="post-item">
+                <div class="media-wrapper">
+                  <img
+                    src="images/blog/post-1.jpg"
+                    alt="amazing caves coverimage"
+                    class="img-fluid"
+                  />
+                </div>
+
+                <div class="content">
+                  <h3>
+                    <a href="single-post.html">Reasons to Smile</a>
+                  </h3>
+                  <p>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non skateboard dolor brunch.
+                  </p>
+                  <a class="btn btn-main" href="single-post.html">
+                    Read more
+                  </a>
+                </div>
+              </div>
+            </article>
+
+            <article class="col-md-3 col-sm-6 col-xs-12 ">
+              <div class="post-item">
+                <div class="media-wrapper">
+                  <img
+                    src="images/blog/post-2.jpg"
+                    alt="amazing caves coverimage"
+                    class="img-fluid"
+                  />
+                </div>
+
+                <div class="content">
+                  <h3>
+                    <a href="single-post.html">A Few Moments</a>
+                  </h3>
+                  <p>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non skateboard dolor brunch.
+                  </p>
+                  <a class="btn btn-main" href="single-post.html">
+                    Read more
+                  </a>
+                </div>
+              </div>
+            </article>
+            <article class="col-md-3 col-sm-6 col-xs-12 ">
+              <div class="post-item">
+                <div class="media-wrapper">
+                  <img
+                    src="images/blog/post-3.jpg"
+                    alt="amazing caves coverimage"
+                    class="img-fluid"
+                  />
+                </div>
+
+                <div class="content">
+                  <h3>
+                    <a href="single-post.html">Hints for Life</a>
+                  </h3>
+                  <p>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non skateboard dolor brunch.
+                  </p>
+                  <a class="btn btn-main" href="single-post.html">
+                    Read more
+                  </a>
+                </div>
+              </div>
+            </article>
+            <article class="col-md-3 col-sm-6 col-xs-12 ">
+              <div class="post-item">
+                <div class="media-wrapper">
+                  <img
+                    src="images/blog/post-3.jpg"
+                    alt="amazing caves coverimage"
+                    class="img-fluid"
+                  />
+                </div>
+
+                <div class="content">
+                  <h3>
+                    <a href="single-post.html">Hints for Life</a>
+                  </h3>
+                  <p>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non skateboard dolor brunch.
+                  </p>
+                  <a class="btn btn-main" href="single-post.html">
+                    Read more
+                  </a>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
