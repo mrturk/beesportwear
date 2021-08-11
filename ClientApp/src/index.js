8e3d90e92dc8604b867e2f1b26cabf34c1eba450
Kaan Bayram <kbayram@bimser.com.tr>
2021-08-11 04:31:06 +0300

    client değişiklikleri

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -9,10 +9,11 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-   <BrowserRouter basename={baseUrl}>
-      <App />
-    </BrowserRouter>,
-  </React.StrictMode>,
+  <React.StrictMode >
+  <BrowserRouter basename={baseUrl}>
+    <App />
+  </BrowserRouter>,
+  </React.StrictMode >,
   rootElement
 );
 
